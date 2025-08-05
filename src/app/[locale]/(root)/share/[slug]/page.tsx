2800945c9ab4b56f22a47334f4e9938c480c0108
DarjaElina <daria.elina98@gmail.com>
2025-08-06 00:07:22 +0300

    fix eslint errors

--- src/app/[locale]/(root)/share/[slug]/page.tsx
+++ src/app/[locale]/(root)/share/[slug]/page.tsx
@@ -3,11 +3,11 @@
 import ResultCard from "@/components/shared/result-card";
 import { PieType } from "@/generated/prisma";
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
-import { results } from "@/lib/constants/questions";
 import { getMetaDataByType } from "@/lib/utils";
 import { PieTypeEnum } from "@/lib/validators";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { getLocalizedResults } from "@/lib/utils";
 
 type SharePageProps = {
   params: Promise<{
@@ -19,6 +19,7 @@ export async function generateMetadata({
   params,
 }: SharePageProps): Promise<Metadata> {
   const { slug } = await params;
+  
 
   const pieSlug = slug.toUpperCase();
 
@@ -34,6 +35,7 @@ export async function generateMetadata({
 
 export default async function SharePage({ params }: SharePageProps) {
   const { slug } = await params;
+  const results = await getLocalizedResults();
 
   const res = results.find((r) => r.type === slug.toUpperCase());
 
